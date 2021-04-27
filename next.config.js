a3816ed1d135291603345550430335ab3b58a394
Andrés Villarreal <andres@villarreal.co.cr>
2021-04-27 18:21:54 +0200

    more h-card stuff

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ module.exports = {
   },
   async rewrites() {
     return [
+      /**
+       * this is so ugly because vercel decided to randomly break
+       * after I deployed new code (without changing this or the dependencies)
+       */
       { source: "/", destination: "/en/cv", locale: false },
       { source: "/en", destination: "/en/cv", locale: false },
       { source: "/es", destination: "/es/cv", locale: false },
